143f61c1229db6198513685782338d2a39bbd6e6
ugochukwu-coder <144166319+ugochukwu-coder@users.noreply.github.com>
2025-02-21 11:20:51 -1200

    Update three

--- src/App.jsx
+++ src/App.jsx
@@ -150,7 +150,10 @@ export default function App() {
               className="textarea"
             />
             <button id="send" onClick={HandleSend} ref={(el) => (buttonsRef.current[0] = el)}>
-              Send
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M22 2L11 13"></path>
+              <path d="M22 2L15 22L11 13L2 9L22 2Z"></path>
+              </svg>
             </button>
           </div>
           <div className="btn">
