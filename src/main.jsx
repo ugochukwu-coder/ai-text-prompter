278a9e90ea5664c053c99fb9856333149d16c44e
ugochukwu-coder <144166319+ugochukwu-coder@users.noreply.github.com>
2025-02-20 15:31:09 -1200

    Second comm

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const originMeta = document.createElement('meta');
+originMeta.httpEquiv = 'origin-trial';
+originMeta.content = import.meta.env.VITE_SUMMARIZER_API_KEY;
+document.head.append(originMeta)
+
+const orgMeta = document.createElement('meta');
+originMeta.httpEquiv = 'origin-trial';
+orgMeta.content = import.meta.env.VITE_TRANSLATOR_API_KEY;
+document.head.append(orgMeta)
+
+const oMeta = document.createElement('meta');
+originMeta.httpEquiv = 'origin-trial';
+oMeta.content = import.meta.env.VITE_LANGUAGE_DETECTOR_API_KEY;
+document.head.append(oMeta)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
